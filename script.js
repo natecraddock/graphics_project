717ab7f5c8353273442e14ab5f0b050fbf38e817
natecraddock <nzcraddock@gmail.com>
2016-04-11 16:16:37 -0600

    Converted all object functions to be prototype functions to save memory!

--- script.js
+++ script.js
@@ -32,159 +32,144 @@ function drawSquare(x, y, w, c) {
 function Point(x, y) {
     this.x = x || 0;
     this.y = y || 0;
-
-    // Functions
-    // Returns true if the points are the same
-    this.compare = function(point){
-        if(this.x == point.x) {
-            if(this.y == point.y) {
-                return true;
-            }
-        }
-        return false;
-    };
-
-    this.distanceTo = function(point) {
-        return Math.sqrt(Math.pow((this.x - point.x), 2) + Math.pow((this.y - point.y), 2));
-    };
-
-    // Sets the point
-    this.setPoint = function(x, y) {
-        this.x = x;
-        this.y = y;
-    }
 }
+
+Point.prototype.compare = function(point) {
+    if(this.x == point.x) {
+        if(this.y == point.y) {
+            return true;
+        }
+    }
+    return false;
+};
+
+// Uses the pythagorean theorem to get the distance between two points
+Point.prototype.distanceTo = function(point) {
+    return Math.sqrt(Math.pow((this.x - point.x), 2) + Math.pow((this.y - point.y), 2));
+};
+
+// Sets the point to requested x and y coordinates
+Point.prototype.setPoint = function(x, y) {
+    this.x = x;
+    this.y = y;
+};
 
 
 // Make this inherit Point for much easier coordinate access. :)
 function Vertex() {
     this.coords = new Point(); // Center of the vert
     this.selected = false;
-
-    // Functions
-    this.draw = function() {
-        var offsetX = this.coords.x - (settings.vertSize / 2);
-        var offsetY = this.coords.y - (settings.vertSize / 2);
-
-        // If less than the threshold, and closest to the mouse...
-        if(this.coords.distanceTo(mouse) <= settings.threshold) {
-            this.selected = true;
-        }
-        else {
-            this.selected = false;
-
-        }
-
-        if (this.selected) {
-            drawSquare(offsetX, offsetY, settings.vertSize, settings.vertColorSelected);
-        } else {
-            drawSquare(offsetX, offsetY, settings.vertSize, settings.vertColor);
-        }
-    }
 }
+
+Vertex.prototype.draw = function() {
+    var offsetX = this.coords.x - (settings.vertSize / 2);
+    var offsetY = this.coords.y - (settings.vertSize / 2);
+
+    // If less than the threshold, and closest to the mouse...
+    this.selected = this.coords.distanceTo(mouse) <= settings.threshold;
+
+    if (this.selected) {
+        drawSquare(offsetX, offsetY, settings.vertSize, settings.vertColorSelected);
+    } else {
+        drawSquare(offsetX, offsetY, settings.vertSize, settings.vertColor);
+    }
+};
+
 
 function Edge() {
-    var verts = []; // We leave this private so it stays fixed to 2 verts
+    this.verts = []; // We leave this private so it stays fixed to 2 verts
+}
 
-    // Functions
-    // Takes two vert objects and pushes them to the array
-    this.setVerts = function(v1, v2) {
-        verts = [];
-        verts.push(v1);
-        verts.push(v2);
-    };
+Edge.prototype.setVerts = function(v1, v2) {
+    this.verts = [];
+    this.verts.push(v1);
+    this.verts.push(v2);
+};
 
-    this.getVerts = function() {
-        return verts;
-    };
+Edge.prototype.getVerts = function() {
+    return this.verts;
+};
 
-    // Returns the specified vert (0 or 1)
-    this.getVert = function(index) {
-        if(index < 0) { index = 0 }
-        if(index > 1) { index = 1 }
+Edge.prototype.getVert = function(index) {
+    if(index < 0) { index = 0 }
+    if(index > 1) { index = 1 }
 
-        return verts[index];
-    };
+    return this.verts[index];
+};
 
-    // Returns the midpoint of the line
-    this.getPos = function() {
-        var x = (verts[1].coords.x + verts[0].coords.x) / 2;
-        var y = (verts[1].coords.y + verts[0].coords.y) / 2;
+Edge.prototype.getPos = function() {
+    var x = (this.verts[1].coords.x + this.verts[0].coords.x) / 2;
+    var y = (this.verts[1].coords.y + this.verts[0].coords.y) / 2;
 
-        return new Point(x, y);
-    };
+    return new Point(x, y);
+};
 
-    // Returns true if the edge is connected
-    this.connected = function(e) {
-        for(var i = 0; i < 2; i++) {
-            for(var j = 0; j < 2; j++) {
-                if(verts[i].coords.compare(e.getVert(j).coords)) {
-                    return true;
-                }
+Edge.prototype.connected = function(e) {
+    for(var i = 0; i < 2; i++) {
+        for(var j = 0; j < 2; j++) {
+            if(this.verts[i].coords.compare(e.getVert(j).coords)) {
+                return true;
             }
-        }
-        return false;
-    };
-
-    // Draws the line
-    this.draw = function() {
-        context.strokeStyle = settings.edgeColor;
-        context.lineWidth = settings.edgeWidth;
-
-        if (verts.length == 2) {
-            context.beginPath();
-            context.moveTo(verts[0].coords.x, verts[0].coords.y);
-            context.lineTo(verts[1].coords.x, verts[1].coords.y);
-            context.stroke();
         }
     }
-}
+    return false;
+};
+
+Edge.prototype.draw = function() {
+    context.strokeStyle = settings.edgeColor;
+    context.lineWidth = settings.edgeWidth;
+
+    if (this.verts.length == 2) {
+        context.beginPath();
+        context.moveTo(this.verts[0].coords.x, this.verts[0].coords.y);
+        context.lineTo(this.verts[1].coords.x, this.verts[1].coords.y);
+        context.stroke();
+    }
+};
+
 
 function Face() {
-    var edges = []; // At least 3 (Must be closed!)
+    this.edges = []; // At least 3 (Must be closed!)
+}
 
-    // Functions
-    // Set the edges
-    this.setEdges = function() {
-        edges = [];
+Face.prototype.setEdges = function() {
+    this.edges = [];
 
-        if(arguments.length > 2) {
-            for(var i = 0; i < arguments.length; i++) {
-                edges.push(arguments[i]);
-            }
+    if(arguments.length > 2) {
+        for(var i = 0; i < arguments.length; i++) {
+            this.edges.push(arguments[i]);
         }
-    };
+    }
+};
 
-    this.validateFace = function() {
-        var validFace = true;
+Face.prototype.validateFace = function() {
+    var validFace = true;
 
-        for(var i = 0; i < edges.length - 1; i++) {
-            if(!edges[i].connected(edges[i + 1])) {
-                validFace = false;
-            }
-        }
-        
-        // Compare first and last as well
-        if(!edges[0].connected((edges[edges.length - 1]))) {
+    for(var i = 0; i < this.edges.length - 1; i++) {
+        if(!this.edges[i].connected(this.edges[i + 1])) {
             validFace = false;
         }
-        
+    }
 
-        return validFace;
-    };
+    // Compare first and last as well
+    if(!this.edges[0].connected((this.edges[this.edges.length - 1]))) {
+        validFace = false;
+    }
 
-    this.draw = function() {
-        if(this.validateFace()) {
-            context.beginPath();
-            for(var i = 0; i < edges.length; i++) {
-                context.lineTo(edges[i].getVert(0).coords.x, edges[i].getVert(0).coords.y);
-                context.lineTo(edges[i].getVert(1).coords.x, edges[i].getVert(1).coords.y);
-            }
-            context.fillStyle = settings.faceColor;
-            context.fill();
+    return validFace;
+};
+
+Face.prototype.draw = function() {
+    if(this.validateFace()) {
+        context.beginPath();
+        for(var i = 0; i < this.edges.length; i++) {
+            context.lineTo(this.edges[i].getVert(0).coords.x, this.edges[i].getVert(0).coords.y);
+            context.lineTo(this.edges[i].getVert(1).coords.x, this.edges[i].getVert(1).coords.y);
         }
-    };
-}
+        context.fillStyle = settings.faceColor;
+        context.fill();
+    }
+};
 
 // Create some geometry for testing purposes
 v = new Vertex();
@@ -254,7 +239,3 @@ canvas.addEventListener('mousemove', draw);
 draw();
 
 console.log(v2.coords.distanceTo(v1.coords));
-
-
-
-
