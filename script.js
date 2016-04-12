1ee801e9228b2fe375adfd4030630afd0d029c58
natecraddock <nzcraddock@gmail.com>
2016-04-11 19:31:39 -0600

    Vertex now inherits from Point
    - Inheritance, removes awful .coords code for much easier and cleaner
    coding
    - Movable verts!
    - Removed unnecessary functions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var context = canvas.getContext('2d');
 
 // Settings
 var settings = {
-    threshold: 8,
+    threshold: 16,
     vertSize: 4,
     vertColor: 'black',
     vertColorSelected: 'orange',
@@ -14,12 +14,21 @@ var settings = {
     faceColorSelected: 'orange'
 };
 
-var mouse = new Point();
+// Temporary array until multiple file structure is enabled
+var hasFocus = undefined;
+
+/*Type{Point}*/
+function Mouse() {
+    this.down = false;
+    this.drag = false;
+}
+
+Mouse.prototype = Object.create(Point.prototype);
 
 // Testing utils
 function displayInfo(info) {
     var infoBox = document.getElementById('info');
-    infoBox.textContent += info;
+    infoBox.textContent = info;
 }
 
 // Canvas utils
@@ -28,7 +37,7 @@ function drawSquare(x, y, w, c) {
     context.fillRect(x, y, w, w);
 }
 
-// Utililty Classes
+// Utility Classes
 function Point(x, y) {
     this.x = x || 0;
     this.y = y || 0;
@@ -54,21 +63,28 @@ Point.prototype.setPoint = function(x, y) {
     this.y = y;
 };
 
-
-// Make this inherit Point for much easier coordinate access. :)
+/*@Type{Point}*/
 function Vertex() {
-    this.coords = new Point(); // Center of the vert
     this.selected = false;
 }
 
+// Inheritance
+Vertex.prototype = Object.create(Point.prototype);
+
 Vertex.prototype.draw = function() {
-    var offsetX = this.coords.x - (settings.vertSize / 2);
-    var offsetY = this.coords.y - (settings.vertSize / 2);
+    var offsetX = this.x - (settings.vertSize / 2);
+    var offsetY = this.y - (settings.vertSize / 2);
 
     // If less than the threshold, and closest to the mouse...
-    this.selected = this.coords.distanceTo(mouse) <= settings.threshold;
+    if(this.distanceTo(Mouse) <= settings.threshold && Mouse.down) {
+        this.selected = !this.selected;
+    }
 
     if (this.selected) {
+        if(Mouse.drag) {
+            this.x = Mouse.x;
+            this.y = Mouse.y;
+        }
         drawSquare(offsetX, offsetY, settings.vertSize, settings.vertColorSelected);
     } else {
         drawSquare(offsetX, offsetY, settings.vertSize, settings.vertColor);
@@ -78,6 +94,7 @@ Vertex.prototype.draw = function() {
 
 function Edge() {
     this.verts = []; // We leave this private so it stays fixed to 2 verts
+    this.selected = false;
 }
 
 Edge.prototype.setVerts = function(v1, v2) {
@@ -86,20 +103,9 @@ Edge.prototype.setVerts = function(v1, v2) {
     this.verts.push(v2);
 };
 
-Edge.prototype.getVerts = function() {
-    return this.verts;
-};
-
-Edge.prototype.getVert = function(index) {
-    if(index < 0) { index = 0 }
-    if(index > 1) { index = 1 }
-
-    return this.verts[index];
-};
-
 Edge.prototype.getPos = function() {
-    var x = (this.verts[1].coords.x + this.verts[0].coords.x) / 2;
-    var y = (this.verts[1].coords.y + this.verts[0].coords.y) / 2;
+    var x = (this.verts[1].x + this.verts[0].x) / 2;
+    var y = (this.verts[1].y + this.verts[0].y) / 2;
 
     return new Point(x, y);
 };
@@ -107,7 +113,7 @@ Edge.prototype.getPos = function() {
 Edge.prototype.connected = function(e) {
     for(var i = 0; i < 2; i++) {
         for(var j = 0; j < 2; j++) {
-            if(this.verts[i].coords.compare(e.getVert(j).coords)) {
+            if(this.verts[i].compare(e.verts[j])) {
                 return true;
             }
         }
@@ -121,15 +127,21 @@ Edge.prototype.draw = function() {
 
     if (this.verts.length == 2) {
         context.beginPath();
-        context.moveTo(this.verts[0].coords.x, this.verts[0].coords.y);
-        context.lineTo(this.verts[1].coords.x, this.verts[1].coords.y);
+        context.moveTo(this.verts[0].x, this.verts[0].y);
+        context.lineTo(this.verts[1].x, this.verts[1].y);
         context.stroke();
+
+        // Draw midpoints
+        vert = new Vertex();
+        vert.setPoint(this.getPos().x, this.getPos().y);
+        vert.draw();
     }
 };
 
 
 function Face() {
     this.edges = []; // At least 3 (Must be closed!)
+    this.selected = false;
 }
 
 Face.prototype.setEdges = function() {
@@ -163,46 +175,42 @@ Face.prototype.draw = function() {
     if(this.validateFace()) {
         context.beginPath();
         for(var i = 0; i < this.edges.length; i++) {
-            context.lineTo(this.edges[i].getVert(0).coords.x, this.edges[i].getVert(0).coords.y);
-            context.lineTo(this.edges[i].getVert(1).coords.x, this.edges[i].getVert(1).coords.y);
+            context.lineTo(this.edges[i].verts[0].x, this.edges[i].verts[0].y);
+            context.lineTo(this.edges[i].verts[1].x, this.edges[i].verts[1].y);
         }
         context.fillStyle = settings.faceColor;
         context.fill();
     }
 };
 
+
 // Create some geometry for testing purposes
 v = new Vertex();
-v.coords.setPoint(30, 55);
+v.setPoint(30, 55);
 
 v1 = new Vertex();
-v1.coords.setPoint(200, 300);
+v1.setPoint(200, 300);
 
 v2 = new Vertex();
-v2.coords.setPoint(300, 100);
+v2.setPoint(300, 100);
 
 v3 = new Vertex();
-v3.coords.setPoint(300, 150);
+v3.setPoint(300, 130);
 
 e = new Edge();
 e.setVerts(v, v1);
-//e.draw();
 
 e1 = new Edge();
 e1.setVerts(v1, v2);
-//e1.draw();
 
 e2 = new Edge();
 e2.setVerts(v2, v);
-//e2.draw();
 
 e3 = new Edge();
 e3.setVerts(v2, v3);
-//e3.draw();
 
 e4 = new Edge();
 e4.setVerts(v1, v3);
-//e4.draw();
 
 f = new Face();
 f.setEdges(e, e1, e2);
@@ -210,32 +218,52 @@ f.setEdges(e, e1, e2);
 f1 = new Face();
 f1.setEdges(e3, e4, e1);
 
-mid = new Vertex();
-mid.coords.x = e4.getPos().x;
-mid.coords.y = e4.getPos().y;
-
-
-function draw(e) {
-    if(e) {
+function draw(event) {
+    if(event) {
         var rect = canvas.getBoundingClientRect();
-        mouse.x = event.clientX - rect.left;
-        mouse.y = event.clientY - rect.top;
+        Mouse.x = event.clientX - rect.left;
+        Mouse.y = event.clientY - rect.top;
+
+        if(event.type == 'mousedown') {
+            Mouse.down = true;
+            hasFocus = event.target;
+        }
+        else if(event.type == 'mouseup') {
+            Mouse.down = false;
+        }
+        else if(event.keyCode == 71 && event.type == 'keydown') {
+            if(hasFocus == canvas) {
+                Mouse.drag = true;
+            }
+        }
+        else if(event.type == "keyup" && event.keyCode == 71) {
+            if(hasFocus == canvas) {
+                Mouse.drag = false;
+            }
+        }
+
     }
 
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    console.log(mouse.x + ", " + mouse.y);
-
+    // Draw all the testing stuff
     f.draw();
     f1.draw();
+    e.draw();
+    e1.draw();
+    e2.draw();
+    e3.draw();
+    e4.draw();
     v.draw();
     v1.draw();
     v2.draw();
     v3.draw();
-    mid.draw();
 }
 
 canvas.addEventListener('mousemove', draw);
+document.addEventListener('mousedown', draw);
+canvas.addEventListener('mouseup', draw);
+document.addEventListener('keydown', draw);
+document.addEventListener('keyup', draw);
 draw();
 
-console.log(v2.coords.distanceTo(v1.coords));
